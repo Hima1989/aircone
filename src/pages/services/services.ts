4a9f50ade0a5badaee166d34841d0457acf3fd05
balubharadwaj <doddibalubharadwaj@gmail.com>
2017-12-15 11:19:49 +0530

    slider fixes and staus fixes added

--- src/pages/services/services.ts
+++ src/pages/services/services.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, App, MenuController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App, MenuController, Platform } from 'ionic-angular';
 import { AirconeProvider } from '../../providers/aircone/aircone';
 import { ServicesHomePage } from '../services-home/services-home';
 
@@ -21,9 +21,19 @@ export class ServicesPage {
   servicesHomePage: any = ServicesHomePage;
   
   
-  constructor(public navCtrl: NavController, public navParams: NavParams, public airconeProvider: AirconeProvider, public app: App, public menu: MenuController) {
+  constructor(public platform: Platform, public navCtrl: NavController, public navParams: NavParams, public airconeProvider: AirconeProvider, public app: App, public menu: MenuController) {
     this.loadServices();
     menu.enable(true);
+    var i = 0;
+    platform.registerBackButtonAction(() => {
+      i++;
+       if (i == 2) {
+        platform.exitApp(); // IF IT'S THE ROOT, EXIT THE APP.        
+       } else {
+         alert("double tap to exit the app")      
+       }
+      
+    });
   }
 
   ionViewDidLoad() {
